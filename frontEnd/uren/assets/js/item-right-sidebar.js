5d271adc000e2189b72f3a56f4ce075cbf32640d
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-04-04 15:18:48 +0530

    load stored items to frontend

---
+++ frontEnd/uren/assets/js/item-right-sidebar.js
@@ -0,0 +1,215 @@
+$(document).ready(function () {
+    $.ajax({
+        url: 'http://localhost:8082/api/v1/item/all',
+        type: 'GET',
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem("token")
+        },
+        success: function (response) {
+            let productContainer = $('.shop-product-wrap'); // Targeting the product container
+
+            response.data.forEach(function (item) {
+                let oldPrice = 2000 + item.itemPrice;
+                let productHTML = `
+                    <div class="col-lg-4">
+                                <div class="product-slide_item">
+                                    <div class="inner-slide">
+                                        <div class="single-product">
+                                            <div class="product-img">
+                                                <a href="../../../pages/single-product-sale.html">
+                                                    <img class="primary-img" src="../${item.itemImage}" alt="${item.itemName}">
+                                                </a>
+                                                <div class="sticker">
+                                                    <span class="sticker">New</span>
+                                                </div>
+                                                <div class="add-actions">
+                                                    <ul>
+                                                        <li><a class="uren-add_cart" href="cart.html" data-toggle="tooltip" data-placement="top" title="Add To Cart"><i
+                                                            class="ion-bag"></i></a>
+                                                        </li>
+                                                        <li><a class="uren-wishlist" href="wishlist.html" data-toggle="tooltip" data-placement="top" title="Add To Wishlist"><i
+                                                            class="ion-android-favorite-outline"></i></a>
+                                                        </li>
+                                                        
+                                                        <li class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><a href="javascript:void(0)" data-toggle="tooltip" data-placement="top" title="Quick View"><i
+                                                            class="ion-android-open"></i></a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div class="product-content">
+                                                <div class="product-desc_info">
+                                                    <h6><a class="product-name" href="../../../pages/single-product-sale.html">${item.itemName}</a></h6>
+                                                    <div class="price-box">
+                                                        <span class="new-price">$122.00</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="list-slide_item">
+                                    <div class="single-product">
+                                        <div class="product-img">
+                                            <a href="single-product.html">
+                                                <img class="primary-img" src="../${item.itemImage}" alt="${item.itemName}">
+                                            </a>
+                                        </div>
+                                        <div class="product-content">
+                                            <div class="product-desc_info">
+
+                                                <h6><a class="product-name" href="single-product.html">${item.itemName}</a></h6>
+                                                <div class="price-box">
+                                                    <span class="new-price">${item.itemPrice}</span>
+                                                </div>
+                                                <div class="product-short_desc">
+                                                    <p>${item.itemDescription}</p>
+                                                </div>
+                                            </div>
+                                            <div class="add-actions">
+                                                <ul>
+                                                    <li><a class="uren-add_cart add-to-cart" href="cart.html" data-toggle="tooltip" data-placement="top" title="Add To Cart"><i class="ion-bag"></i></a>
+                                                    </li>
+                                                    <li><a class="uren-wishlist add-to-wishlist" href="wishlist.html" data-toggle="tooltip" data-placement="top" title="Add To Wishlist"><i
+                                                        class="ion-android-favorite-outline"></i></a>
+                                                    </li>
+                                                    <li class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><a href="javascript:void(0)" data-toggle="tooltip" data-placement="top" title="Quick View"><i
+                                                        class="ion-android-open"></i></a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="modal fade modal-wrapper" id="exampleModalCenter">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <div class="modal-inner-area sp-area row">
+                            <div class="col-lg-5">
+                                <div class="sp-img_area">
+                                    <div class="sp-img_slider slick-img-slider uren-slick-slider" data-slick-options='{
+                                    "slidesToShow": 1,
+                                    "arrows": false,
+                                    "fade": true,
+                                    "draggable": false,
+                                    "swipe": false,
+                                    "asNavFor": ".sp-img_slider-nav"
+                                    }'>
+                                        <div class="single-slide red">
+                                            <img src="../${item.itemImage}" alt="Uren's Product Image">
+                                        </div>
+                                    </div>
+                                    <div class="sp-img_slider-nav slick-slider-nav uren-slick-slider slider-navigation_style-3" data-slick-options='{
+                                   "slidesToShow": 4,
+                                    "asNavFor": ".sp-img_slider",
+                                   "focusOnSelect": true,
+                                   "arrows" : true,
+                                   "spaceBetween": 30
+                                  }' data-slick-responsive='[
+                                    {"breakpoint":1501, "settings": {"slidesToShow": 3}},
+                                    {"breakpoint":992, "settings": {"slidesToShow": 4}},
+                                    {"breakpoint":768, "settings": {"slidesToShow": 3}},
+                                    {"breakpoint":575, "settings": {"slidesToShow": 2}}
+                                ]'>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-7 col-lg-6">
+                                <div class="sp-content">
+                                    <div class="sp-heading">
+                                        <h5><a href="#">${item.itemName}</a></h5>
+                                    </div>
+                                    <div class="price-box">
+                                        <span class="new-price new-price-2">${item.itemPrice}</span>
+                                        <span class="old-price">${oldPrice}</span>
+                                    </div>
+                                    <div class="sp-essential_stuff">
+                                        <ul>
+                                            <li>Vehicle Moodel <a href="javascript:void(0)">${item.vehicleModel}</a></li>                                   <li>Fuel Type <a href="javascript:void(0)">${item.fuelType}</a></li>
+                                            <li>Product Code: <a href="javascript:void(0)">${item.itemId}</a></li>
+                                            <li>Reward Points: <a href="javascript:void(0)">100</a></li>
+                                            <li>Availability: <a href="javascript:void(0)">In Stock</a></li>
+                                        </ul>
+                                    </div>
+                                    
+                                    <div class="quantity">
+                                        <label>Quantity</label>
+                                        <div class="cart-plus-minus">
+                                            <input class="cart-plus-minus-box" value="1" type="text">
+                                            <div class="dec qtybutton"><i class="fa fa-angle-down"></i></div>
+                                            <div class="inc qtybutton"><i class="fa fa-angle-up"></i></div>
+                                        </div>
+                                    </div>
+                                    <div class="uren-group_btn">
+                                        <ul>
+                                            <li><a href="cart.html" class="add-to_cart">Cart To Cart</a></li>
+                                            <li><a href="cart.html"><i class="ion-android-favorite-outline"></i></a></li>
+                                            <li><a href="cart.html"><i class="ion-ios-shuffle-strong"></i></a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="uren-tag-line">
+                                        <h6>Tags:</h6>
+                                        <a href="javascript:void(0)">Shop</a>,
+                                        <a href="javascript:void(0)">Item</a>,
+                                        <a href="javascript:void(0)">${item.itemName}</a>
+                                    </div>
+                                    <div class="uren-social_link">
+                                        <ul>
+                                            <li class="facebook">
+                                                <a href="https://www.facebook.com/" data-toggle="tooltip" target="_blank" title="Facebook">
+                                                    <i class="fab fa-facebook"></i>
+                                                </a>
+                                            </li>
+                                            <li class="twitter">
+                                                <a href="https://twitter.com/" data-toggle="tooltip" target="_blank" title="Twitter">
+                                                    <i class="fab fa-twitter-square"></i>
+                                                </a>
+                                            </li>
+                                            <li class="youtube">
+                                                <a href="https://www.youtube.com/" data-toggle="tooltip" target="_blank" title="Youtube">
+                                                    <i class="fab fa-youtube"></i>
+                                                </a>
+                                            </li>
+                                            <li class="google-plus">
+                                                <a href="https://www.plus.google.com/discover" data-toggle="tooltip" target="_blank" title="Google Plus">
+                                                    <i class="fab fa-google-plus"></i>
+                                                </a>
+                                            </li>
+                                            <li class="instagram">
+                                                <a href="https://rss.com/" data-toggle="tooltip" target="_blank" title="Instagram">
+                                                    <i class="fab fa-instagram"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+`;
+
+                productContainer.append(productHTML); // Append the dynamically created product card
+            });
+        },
+        error: function (xhr) {
+            console.log("Error loading items", xhr);
+        }
+    });
+});
+
+$(".add-to_cart").click (function (e){
+    e.preventDefault();
+    console.log("Add to cart button clicked");
+
+})
+
+
